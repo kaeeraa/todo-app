faad7df2a63d8494b9bd163fc2eeac8ab9948d98
kaeeraa <kaeeraa@nebula-nook.ru>
2025-06-25 11:11:05 +0300

    fix(task): fix type check
    Signed-off-by: kaeeraa <kaeeraa@nebula-nook.ru>

--- src/todo-app/task.py
+++ src/todo-app/task.py
@@ -1,5 +1,4 @@
 from typing import Optional
-
 from textual.widgets import Checkbox
 
 
@@ -8,9 +7,9 @@ class Task(Checkbox):
         super().__init__(label=goal, value=completed)
 
     def updateTask(
-        self, goal: Optional[str] = "", completed: Optional[bool] = None
+        self, goal: Optional[str] = None, completed: Optional[bool] = None
     ) -> None:
-        if goal:
+        if goal is not None:
             self.label = goal
         if completed is not None:
             self.value = completed
